468aecc5b15c33ea469b9c87a230e7bf363d46b6
imadsaad <imad.alhaj.saad@gmail.com>
2021-11-26 15:23:25 +0200

    initial

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const mongoose = require("mongoose");
 const Student = require("./Models/Student");
 const basicAuth = require("express-basic-auth");
 const app = express();
-
 const dbURL =
   "mongodb+srv://mydb:93928@cluster0.g0he0.mongodb.net/Test?retryWrites=true&w=majority";
 
 //connecting to Database
-//const dbURL ="mongodb+srv://mydb:93928@cluster0.g0he0.mongodb.net/Test?retryWrites=true&w=majority";
-
-// link for postman collection : https://www.getpostman.com/collections/d8b65b634e89029b3461
 
 mongoose
   .connect(dbURL)
@@ -23,7 +19,7 @@ mongoose
 app.use(express.json());
 
 // middleware to check auth
-// username : admin ; password:supersecret
+
 
 app.use(
   basicAuth({
